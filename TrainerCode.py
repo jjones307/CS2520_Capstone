9116876baaa86f0c17466c814159aa5e5ba00c20
jjones <jjones@cpp.edu>
2022-05-10 17:43:51 -0700

    Fixed infinite loop condition

--- TrainerCode.py
+++ TrainerCode.py
@@ -3,39 +3,40 @@ from CsvReader import reader
 
 
 def main():
-    usr_input = input("Enter the text you would like to analyze:\n")
+    user_input = input("Enter the text you would like to analyze:\n")
     vowels = "aeiou"
     vowel_frequency = dict()
     vowel_group_frequency = dict()
     consonant_frequency = dict()
 
-    usr_input = usr_input.lower()
-    usr_input = [char for char in usr_input if char in string.ascii_lowercase]
-    usr_input = "".join(usr_input)
-    print(usr_input)
+    user_input = user_input.lower()
+    user_input = [char for char in user_input if char in string.ascii_lowercase]
+    user_input = "".join(user_input)
+    print(user_input)
     i = 0
 
-    while i < len(usr_input):
-        if usr_input[i] not in vowels:
+    while i < len(user_input):
+        #print(i)
+        if user_input[i] not in vowels:
             try:
-                consonant_frequency[usr_input[i]] += 1
+                consonant_frequency[user_input[i]] += 1
             except KeyError:
-                consonant_frequency[usr_input[i]] = 1
+                consonant_frequency[user_input[i]] = 1
             finally:
                 i += 1
-        elif usr_input[i] in vowels:
-            if i+1 < len(usr_input) and usr_input[i+1] not in vowels:
+        elif user_input[i] in vowels:
+            if (i+1 < len(user_input) and user_input[i+1] not in vowels) or i == len(user_input) - 1:
                 try:
-                    vowel_frequency[usr_input[i]] += 1
+                    vowel_frequency[user_input[i]] += 1
                 except KeyError:
-                    vowel_frequency[usr_input[i]] = 1
+                    vowel_frequency[user_input[i]] = 1
                 finally:
                     i += 1
-            elif i+1 < len(usr_input) and usr_input[i+1] in vowels:
-                vowel_group = usr_input[i]
+            elif i+1 < len(user_input) and user_input[i+1] in vowels:
+                vowel_group = user_input[i]
                 j = i+1
-                while j < len(usr_input) and usr_input[j] in vowels:
-                    vowel_group += usr_input[j]
+                while j < len(user_input) and user_input[j] in vowels:
+                    vowel_group += user_input[j]
                     j += 1
                 try:
                     vowel_group_frequency[vowel_group] += 1
