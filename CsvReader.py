b1ef86c62f4b7542d2121d35dc1defba4228239e
Uziel Gaeta <ugaeta@cpp.edu>
2022-05-12 08:56:08 -0700

    changed while loop to for loop

--- CsvReader.py
+++ CsvReader.py
@@ -1,6 +1,20 @@
 def reader():
     file_obj = open("names.csv", 'r')
     for line in file_obj:
-        string_obj = line.rstrip()
+        string_obj = line.strip().replace(' ', '')
         yield string_obj.split(',')
     file_obj.close()
+
+
+def main():
+    str_obj = reader()
+    while True:
+        try:
+            print(next(str_obj))
+        except StopIteration:
+            break
+    print("end of program")
+
+
+if __name__ == '__main__':
+    main()
--- TrainerCode.py
+++ TrainerCode.py
@@ -12,25 +12,19 @@ def main():
     usr_input = usr_input.lower()
     usr_input = [char for char in usr_input if char in string.ascii_lowercase]
     usr_input = "".join(usr_input)
-    print(usr_input)
-    i = 0
 
-    while i < len(usr_input):
+    for i in range(len(usr_input)):
         if usr_input[i] not in vowels:
             try:
                 consonant_frequency[usr_input[i]] += 1
             except KeyError:
                 consonant_frequency[usr_input[i]] = 1
-            finally:
-                i += 1
-        elif usr_input[i] in vowels:
+        else:
             if i+1 < len(usr_input) and usr_input[i+1] not in vowels:
                 try:
                     vowel_frequency[usr_input[i]] += 1
                 except KeyError:
                     vowel_frequency[usr_input[i]] = 1
-                finally:
-                    i += 1
             elif i+1 < len(usr_input) and usr_input[i+1] in vowels:
                 vowel_group = usr_input[i]
                 j = i+1
@@ -41,8 +35,6 @@ def main():
                     vowel_group_frequency[vowel_group] += 1
                 except KeyError:
                     vowel_group_frequency[vowel_group] = 1
-                finally:
-                    i = j
 
     print(list(vowel_frequency.items()))
     print(list(vowel_group_frequency.items()))
