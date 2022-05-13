d4da2efb015df6999e8970df2b791f5d9701a30d
Uziel Gaeta <ugaeta@cpp.edu>
2022-05-12 18:52:52 -0700

    Fixed small syntax error

--- TrainerCode.py
+++ TrainerCode.py
@@ -3,33 +3,32 @@ from CsvReader import reader
 
 
 def main():
-    user_input = input("Enter the text you would like to analyze:\n")
+    usr_input = input("Enter the text you would like to analyze:\n").lower()
     vowels = "aeiou"
     vowel_frequency = dict()
     vowel_group_frequency = dict()
     consonant_frequency = dict()
 
-    usr_input = usr_input.lower()
     usr_input = [char for char in usr_input if char in string.ascii_lowercase]
     usr_input = "".join(usr_input)
 
     for i in range(len(usr_input)):
         if usr_input[i] not in vowels:
             try:
-                consonant_frequency[user_input[i]] += 1
+                consonant_frequency[usr_input[i]] += 1
             except KeyError:
                 consonant_frequency[usr_input[i]] = 1
         else:
             if i+1 < len(usr_input) and usr_input[i+1] not in vowels:
                 try:
-                    vowel_frequency[user_input[i]] += 1
+                    vowel_frequency[usr_input[i]] += 1
                 except KeyError:
                     vowel_frequency[usr_input[i]] = 1
             elif i+1 < len(usr_input) and usr_input[i+1] in vowels:
                 vowel_group = usr_input[i]
                 j = i+1
-                while j < len(user_input) and user_input[j] in vowels:
-                    vowel_group += user_input[j]
+                while j < len(usr_input) and usr_input[j] in vowels:
+                    vowel_group += usr_input[j]
                     j += 1
                 try:
                     vowel_group_frequency[vowel_group] += 1
