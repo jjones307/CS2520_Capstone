915cb2df6e7f388137d60badf2afd9b2e81c5453
Uziel Gaeta <ugaeta@cpp.edu>
2022-05-14 23:15:46 -0700

    Entered German language

--- Generator.py
+++ Generator.py
@@ -2,7 +2,6 @@ from CsvReader import reader
 import random
 
 
-
 def generate(lang_choice):
     lang_file = lang_choice.lower() + ".txt"
     vowel_frequency = dict()
@@ -11,9 +10,9 @@ def generate(lang_choice):
     consonant_group_frequency = dict()
     rel_frequencies = dict()
     n = 1
-    
+
     inf = reader(lang_file)
-    counts=[vowel_frequency, consonant_frequency, vowel_group_frequency, consonant_group_frequency, rel_frequencies]
+    counts = [vowel_frequency, consonant_frequency, vowel_group_frequency, consonant_group_frequency, rel_frequencies]
     c_index = 0
 
     n = int(next(inf)[0])
@@ -21,11 +20,11 @@ def generate(lang_choice):
         line = next(inf)
         if line[0] == "#":
             c_index += 1
-        else:            
+        else:
             # Division by n occurs here in order to produce an average frequency
-            counts[c_index][line[0] ] = float(line[1])/n
+            counts[c_index][line[0]] = float(line[1]) / n
 
-    name_length = random.randint(5,8)
+    name_length = random.randint(5, 8)
     generated_name = ""
     next_letter = ""
 
@@ -33,30 +32,36 @@ def generate(lang_choice):
     choice = random.choices(v_or_c, weights=[rel_frequencies["v"], rel_frequencies["c"]])
     next_letter = choice[0]
 
-    while(len(generated_name) < name_length):
+    while (len(generated_name) < name_length):
         if next_letter == 'v':
-            choice = random.choices(['v', 'vg'], weights = [rel_frequencies['v'] - rel_frequencies['vg'], rel_frequencies['vg']])[0]
+            choice = \
+            random.choices(['v', 'vg'], weights=[rel_frequencies['v'] - rel_frequencies['vg'], rel_frequencies['vg']])[
+                0]
             if choice == 'v':
                 letter_to_add = random.choices(list(vowel_frequency.keys()), weights=list(vowel_frequency.values()))[0]
                 generated_name += letter_to_add
             else:
-                group_to_add = random.choices(list(vowel_group_frequency.keys()), weights=list(vowel_group_frequency.values()))[0]
+                group_to_add = \
+                random.choices(list(vowel_group_frequency.keys()), weights=list(vowel_group_frequency.values()))[0]
                 generated_name += group_to_add
             next_letter = 'c'
         elif next_letter == 'c':
-            choice = random.choices(['c', 'cg'], weights = [rel_frequencies['c'] - rel_frequencies['cg'], rel_frequencies['cg']])[0]
+            choice = \
+            random.choices(['c', 'cg'], weights=[rel_frequencies['c'] - rel_frequencies['cg'], rel_frequencies['cg']])[
+                0]
             if choice == 'c':
-                letter_to_add = random.choices(list(consonant_frequency.keys()), weights=list(consonant_frequency.values()))[0]
+                letter_to_add = \
+                random.choices(list(consonant_frequency.keys()), weights=list(consonant_frequency.values()))[0]
                 generated_name += letter_to_add
             else:
-                group_to_add = random.choices(list(consonant_group_frequency.keys()), weights=list(consonant_group_frequency.values()))[0]
+                group_to_add = random.choices(list(consonant_group_frequency.keys()),
+                                              weights=list(consonant_group_frequency.values()))[0]
                 generated_name += group_to_add
             next_letter = 'v'
-    
+
     return generated_name
 
+
 # Just here for testing purposes, we'll have to comment this out in the finished product
-print(generate("french"))
-    
-
-
+if __name__ == '__main__':
+    print(generate("german"))
--- TrainerCode.py
+++ TrainerCode.py
@@ -1,5 +1,6 @@
 import string
-from CsvReader import reader 
+from CsvReader import reader
+
 
 def main():
     print("Which language is the text in? \n * German\n * French\n * Spanish")
@@ -13,7 +14,7 @@ def main():
     special_characters = "çñ"
     vowel_total = 0
     cons_total = 0
-    uncounted = 0  
+    uncounted = 0
 
     # These are used for finding the counts in the given text  
     vowel_counts = dict()
@@ -34,7 +35,8 @@ def main():
 
     try:
         inf = reader(lang_file)
-        counts = [vowel_frequency, consonant_frequency, vowel_group_frequency, consonant_group_frequency, rel_frequencies]
+        counts = [vowel_frequency, consonant_frequency, vowel_group_frequency, consonant_group_frequency,
+                  rel_frequencies]
         c_index = 0
 
         n = int(next(inf)[0])
@@ -42,7 +44,7 @@ def main():
             line = next(inf)
             if line[0] == "#":
                 c_index += 1
-            else:            
+            else:
                 counts[c_index][line[0]] = float(line[1])
     except FileNotFoundError:
         pass
@@ -50,9 +52,10 @@ def main():
     user_input = user_input.lower()
     # Spaces are left in so that the last letter in a word and the first letter in the next word don't get
     # counted as a letter grouping
-    user_input = [char for char in user_input if char in string.ascii_lowercase or char in vowels or char in special_characters or char == " "]
+    user_input = [char for char in user_input if
+                  char in string.ascii_lowercase or char in vowels or char in special_characters or char == " "]
     user_input = "".join(user_input)
-    #print(user_input)
+    # print(user_input)
     i = 0
 
     # This first pass through is a simple count of the vowels and consonants in the text. These counts are used to determine
@@ -66,13 +69,13 @@ def main():
         else:
             uncounted += 1
 
-    # This second pass through counts how often each vowel, consonant, vowel group, and consonant group appears in the test. 
+    # This second pass through counts how often each vowel, consonant, vowel group, and consonant group appears in the test.
     # This is done so that later on we can use weighted random choices construct letter groupings which mimic those of the target language.
     while i < len(user_input):
         if user_input[i] == " ":
             i += 1
             pass
-        elif user_input[i] not in vowels: 
+        elif user_input[i] not in vowels:
             if i == len(user_input) - 1:
                 try:
                     consonant_counts[user_input[i]] += 1
@@ -80,18 +83,20 @@ def main():
                     consonant_counts[user_input[i]] = 1
                 finally:
                     i += 1
-            elif i+1 < len(user_input):
-                if user_input[i+1] in vowels or (user_input[i+1] not in string.ascii_lowercase and user_input[i+1] not in special_characters):
+            elif i + 1 < len(user_input):
+                if user_input[i + 1] in vowels or (user_input[i + 1] not in string.ascii_lowercase and user_input[
+                    i + 1] not in special_characters):
                     try:
                         consonant_counts[user_input[i]] += 1
                     except KeyError:
                         consonant_counts[user_input[i]] = 1
                     finally:
                         i += 1
-                elif user_input[i+1] not in vowels:
+                elif user_input[i + 1] not in vowels:
                     consonant_group = user_input[i]
-                    j = i+1
-                    while j < len(user_input) and user_input[j] not in vowels and (user_input[j] in string.ascii_lowercase or user_input[j] in special_characters):                    
+                    j = i + 1
+                    while j < len(user_input) and user_input[j] not in vowels and (
+                            user_input[j] in string.ascii_lowercase or user_input[j] in special_characters):
                         consonant_group += user_input[j]
                         j += 1
                     try:
@@ -108,18 +113,18 @@ def main():
                     vowel_counts[user_input[i]] = 1
                 finally:
                     i += 1
-            elif i+1 < len(user_input):
-                if user_input[i+1] not in vowels:
+            elif i + 1 < len(user_input):
+                if user_input[i + 1] not in vowels:
                     try:
                         vowel_counts[user_input[i]] += 1
                     except KeyError:
                         vowel_counts[user_input[i]] = 1
                     finally:
                         i += 1
-                elif user_input[i+1] in vowels:
+                elif user_input[i + 1] in vowels:
                     vowel_group = user_input[i]
-                    j = i+1
-                    while j < len(user_input) and user_input[j] in vowels:                    
+                    j = i + 1
+                    while j < len(user_input) and user_input[j] in vowels:
                         vowel_group += user_input[j]
                         j += 1
                     try:
@@ -127,10 +132,10 @@ def main():
                     except KeyError:
                         vowel_group_counts[vowel_group] = 1
                     finally:
-                        i = j  
+                        i = j
 
-    # This series of loops finds the frequency with which different vowels and consonants appear next to each other.
-    # The goal is to use the frequency of adjacency to produce more readable outputs that more closely resemble the 
+                        # This series of loops finds the frequency with which different vowels and consonants appear next to each other.
+    # The goal is to use the frequency of adjacency to produce more readable outputs that more closely resemble the
     # target language.
 
     '''
@@ -151,11 +156,9 @@ def main():
     letter_counts = [list(vowel_counts.keys()), list(consonant_counts.keys()), list(vowel_group_counts.keys()), list(consonant_group_counts.keys())]
     # adjacencies is a dictionary of dictionaries
     adjacencies = dict()
-
     for key_list in letter_counts:
         for key in key_list:
             adjacencies[key] = dict()
-
     i = 0
     for i in range(len(user_input)):
         if user_input[i] == " ":
@@ -172,13 +175,13 @@ def main():
                 except KeyError:
                     adjacencies[user_input[i]][user_input[i+1]] = 1
     '''
-    ################################### Nothing between this bar and the one above actually does anything yet ###################################            
+    ################################### Nothing between this bar and the one above actually does anything yet ###################################
 
-    rel_vowel_frequency = vowel_total/(len(user_input) - uncounted)
-    rel_consonant_frequency = cons_total/(len(user_input) - uncounted)
-    rel_vowel_group_frequency = (sum(vowel_group_counts.values()))/(len(user_input) - uncounted)
-    rel_cons_group_frequency = (sum(consonant_group_counts.values()))/(len(user_input) - uncounted)
-    
+    rel_vowel_frequency = vowel_total / (len(user_input) - uncounted)
+    rel_consonant_frequency = cons_total / (len(user_input) - uncounted)
+    rel_vowel_group_frequency = (sum(vowel_group_counts.values())) / (len(user_input) - uncounted)
+    rel_cons_group_frequency = (sum(consonant_group_counts.values())) / (len(user_input) - uncounted)
+
     try:
         rel_frequencies["v"] += rel_vowel_frequency
     except KeyError:
@@ -195,7 +198,7 @@ def main():
         rel_frequencies["cg"] += rel_cons_group_frequency
     except KeyError:
         rel_frequencies["cg"] = rel_cons_group_frequency
-    
+
     # This block divides the counts for the provided text by the length of the text to find the
     # relative frequency of each letter in the text.
     # It then adds this relative frequency to the values read in from the file so they can be
@@ -251,6 +254,7 @@ def main():
     outf.write("#\n")
 
     outf.close()
+
 
 if __name__ == '__main__':
     main()
